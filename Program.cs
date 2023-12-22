3066141b34ed90a630432d1c1a22523bf7567473
Anastasiia <anastasiia98@internet.ru>
2023-12-22 13:21:36 +0400

    Дз3

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+
+using System;
+
+
+
+class Program
+
+{
+    static void Main()
+
+
+    {
+
+        int[] array = { 1, 2, 3, 4, 5 };
+        
+        PrintArrayReversely(array, array.Length - 1);
+
+    }
+    static void PrintArrayReversely(int[] array, int currentIndex)
+
+    {
+
+        if (currentIndex < 0)
+
+        {
+    
+            return;
+
+        }
+
+        Console.WriteLine(array[currentIndex]);
+
+        PrintArrayReversely(array, currentIndex - 1);
+
+    }
+
+}
+
+
+
+        
+
+
+
+
+
+        
+
+        
+
+
+
+
+
+        
